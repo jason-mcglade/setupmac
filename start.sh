e493deb08c01cbe7b9cce0a9badef41c3805ce1e
Jason McGlade <jason.mcglade@jobseeker.com.au>
2018-01-18 18:01:28 +1100

    Check for homebrew before running

--- start.sh
+++ start.sh
@@ -37,8 +37,6 @@ which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    brew update
 fi
 
 brew install openssl
